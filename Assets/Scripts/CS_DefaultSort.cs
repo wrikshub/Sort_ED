641a95b3ca1e492a849275e82246c7697fdf2ad2
Erik Engström <wrikseng@gmail.com>
2022-11-02 20:47:39 +0100

    j

--- Assets/Scripts/CS_DefaultSort.cs
+++ Assets/Scripts/CS_DefaultSort.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Profiling;
 
 [CreateAssetMenu(menuName = "Sort/CS Default Sort")]
 public class CS_DefaultSort : Sorter
@@ -10,6 +11,8 @@ public class CS_DefaultSort : Sorter
     public override void Sort(ref List<Ball> balls)
     {
         base.Sort(ref balls);
+        Profiler.BeginSample("Trial");
         balls = balls.OrderBy(balls => balls.DstFromTarget).ToList();
+        Profiler.EndSample();
     }
 }
