3b9a910ceef5aa018a16c8ae1b93d17c04a31b3c
Erik Engström <39408961+wrikshub@users.noreply.github.com>
2022-11-27 17:35:59 +0100

    j

--- Assets/Scripts/SortManager.cs
+++ Assets/Scripts/SortManager.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
